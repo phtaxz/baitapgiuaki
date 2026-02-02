12d3bb9ef38203b1a05e90e9c1fceece569780c8
phta2702 <tuananh27022005@gmail.com>
2026-02-02 10:00:30 +0700

    Delete DenKeoQuan_GK directory

--- DenKeoQuan_GK/script.js
+++
@@ -1,9 +0,0 @@
-const carousel = document.getElementById('denKeoQuan');
-
-carousel.addEventListener('mouseenter', () => {
-    carousel.style.animationPlayState = 'paused';
-});
-
-carousel.addEventListener('mouseleave', () => {
-    carousel.style.animationPlayState = 'running';
-});
